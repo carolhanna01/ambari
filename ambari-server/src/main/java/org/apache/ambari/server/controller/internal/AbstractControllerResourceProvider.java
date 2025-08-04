c19939e50791fe278cff8627ce976eea1494fa02
carolhanna01 <carol.z.hanna@gmail.com>
2025-08-04 22:10:58 +0100

    Reconstructing bug

--- ambari-server/src/main/java/org/apache/ambari/server/controller/internal/AbstractControllerResourceProvider.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/internal/AbstractControllerResourceProvider.java
@@ -125,33 +125,26 @@ public abstract class AbstractControllerResourceProvider extends AbstractResourc
   }
 
   /**
-   * Extracting given query_paramater value from the predicate
+   * Extracting given query_paramater value from the predicate(parsed http body)
    * @param queryParameterId
    * @param predicate
    * @return
    */
   protected static Object getQueryParameterValue(String queryParameterId, Predicate predicate) {
 
-    Object result = null;
-
     if (predicate instanceof ArrayPredicate) {
       ArrayPredicate arrayPredicate  = (ArrayPredicate) predicate;
       for (Predicate predicateItem : arrayPredicate.getPredicates()) {
-          if (predicateItem instanceof EqualsPredicate) {
-            EqualsPredicate equalsPredicate =
-                (EqualsPredicate) predicateItem;
-            if (queryParameterId.equals(equalsPredicate.getPropertyId())) {
-              return equalsPredicate.getValue();
-            }
-          } else {
-            result = getQueryParameterValue(queryParameterId, predicateItem);
-            if (result != null) {
-              return result;
+        if (predicateItem instanceof ComparisonPredicate) {
+          EqualsPredicate equalsPredicate =
+              (EqualsPredicate) predicateItem;
+          if (queryParameterId.equals(equalsPredicate.getPropertyId())) {
+            return equalsPredicate.getValue();
           }
         }
       }
 
     }
-    return result;
+    return null;
   }
 }
